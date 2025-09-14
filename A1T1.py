0a462be2909fee8895f83b2f0588bb2dfdcee889
Devam Surti <surtidevam20022007@gmail.com>
2025-09-14 16:37:44 +0530

    Create A1T1.py

---
+++ A1T1.py
@@ -0,0 +1,12 @@
+a=input("Enter first number")
+b=input("Enter second number")
+a=float(a)
+b=float(b)
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition is",add)
+print("Subtraction is",sub)
+print("Multiplication is",mul)
+print("Division is",div)
